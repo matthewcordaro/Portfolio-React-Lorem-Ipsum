3a58e4dfe9b4e6d0901f12087c2f6f01748f2abc
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-04-08 22:51:59 -0400

    Created lorem generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,41 @@
+import React, { useState } from "react"
+import data from "./data"
+import { nanoid } from "nanoid"
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
-};
-export default App;
+  const [text, setText] = useState([data[0]])
+  const [count, setCount] = useState(1)
+
+  function submissionHandler(e) {
+    e.preventDefault()
+    setText(data.slice(0, count))
+  }
+
+  return (
+    <section className='section-center'>
+      <h4>Tired of boring Lorem Ipsum?</h4>
+      <form className='lorem-form' onSubmit={submissionHandler}>
+        <label htmlFor='amount'>Paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          min={1}
+          step={1}
+          max={data.length}
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button type='submit' className='btn'>
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((paragraph) => {
+          return <p key={nanoid()}>{paragraph}</p>
+        })}
+      </article>
+    </section>
+  )
+}
+export default App
